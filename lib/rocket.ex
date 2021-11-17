5b70a2d7214b2408c16bebf8938ac162e560cabd
kevinzepwa <zepwakevin8@gmail.com>
2021-11-17 09:22:59 +0300

    minor adjustments

--- lib/rocket.ex
+++ lib/rocket.ex
@@ -9,6 +9,8 @@ defmodule Rocket do
   ## Example
       iex> Rocket.total_fuel(28801, [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 9.807}])
       36778
+      iex> Rocket.total_fuel(75432, [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62},{:land, 3.711}, {:launch, 3.711}, {:land, 9.807}])
+      121785
   """
   @spec total_fuel(float(), [
           {:launch, float()},
@@ -25,5 +27,25 @@ defmodule Rocket do
     destination_launch_fuel(mass, destination_gravity) +
       destination_land_fuel(mass, destination_gravity) + return_launch_fuel(mass, return_gravity) +
       return_land_fuel(mass, return_gravity)
+  end
+
+  @spec total_fuel(float(), [
+          {:launch, float()},
+          {:land, float()},
+          {:launch, float()},
+          {:land, float()},
+          {:launch, float()},
+          {:land, float()}
+        ]) :: integer()
+  def total_fuel(mass, [
+        {:launch, return_gravity},
+        {:land, destination_gravity},
+        {:launch, destination_gravity},
+        {:land, destination_two_gravity},
+        {:launch, destination_two_gravity},
+        {:land, return_gravity}
+      ]) do
+        destination_launch_fuel(mass, destination_gravity) +
+      destination_land_fuel(mass, destination_gravity) + return_launch_fuel(mass, return_gravity) + destination_land_fuel(mass, destination_two_gravity) + destination_launch_fuel(mass, destination_two_gravity) + return_land_fuel(mass, return_gravity)
   end
 end
--- test/rocket_test.exs
+++ test/rocket_test.exs
@@ -3,12 +3,32 @@ defmodule RocketTest do
   doctest Rocket
   doctest Rocket.Fuel
 
-  test "total_final_fuel" do
+  test "total_final_fuel_to_moon" do
     assert Rocket.total_fuel(28801, [
              {:launch, 9.807},
              {:land, 1.62},
              {:launch, 1.62},
              {:land, 9.807}
            ]) == 36778
+  end
+
+  test "total_final_fuel_to_mars" do
+    assert Rocket.total_fuel(75432, [
+             {:launch, 9.807},
+             {:land, 3.711},
+             {:launch, 3.711},
+             {:land, 9.807}
+           ]) == 112_212
+  end
+
+  test "total_final_fuel_to_moon_and_mars" do
+    assert Rocket.total_fuel(75432, [
+             {:launch, 9.807},
+             {:land, 1.62},
+             {:launch, 1.62},
+             {:land, 3.711},
+             {:launch, 3.711},
+             {:land, 9.807}
+           ]) == 121_785
   end
 end
